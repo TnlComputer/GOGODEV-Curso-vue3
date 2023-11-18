03b1731817faa46dec42c730748a0caf50d17245
Jorge Martinez <tnlcomputer@gmail.com>
2023-11-18 11:34:34 -0300

    video 3 -  Episodio 2 - Componentes

---
+++ helloword/src/components/PostCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="post-card">
+    <h3>Titulo</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis alias tempore nesciunt ipsa suscipit labore
+      voluptatum sint vitae voluptatibus earum eveniet, officia soluta quia officiis, harum itaque nemo. Magnam, expedita!
+    </p>
+    <PostButton />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import PostButton from './PostButton.vue'
+</script>
+
+<style scoped>
+.post-card {
+  width: 20%;
+  border-radius: 15px;
+  padding: 10px;
+  margin: 5px;
+  border: 1px solid #ccc;
+}
+</style>
+
