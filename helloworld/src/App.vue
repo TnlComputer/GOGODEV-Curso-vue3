4957bbdd98c7c507e6ac9a0cb11bc1624d4834c4
Jorge Martinez <tnlcomputer@gmail.com>
2023-11-20 19:05:25 -0300

    reestructuracion

--- helloworld/src/App.vue
+++ helloworld/src/App.vue
@@ -1,24 +1,34 @@
 <template>
-  <h1>Listado de Posts:</h1>
-  <PostContainer />
-  <CounterOption />
-  <CounterComposition />
-  <ExampleSelective />
-  <ExampleVFor />
-  <ClassBind />
-  <StyleBind />
-  <ModelForm />
+  <!-- <h1>Listado de Posts:</h1> -->
+  <!-- <PostContainer /> -->
+  <!-- <CounterOption /> -->
+  <!-- <CounterComposition /> -->
+  <!-- <ExampleSelective /> -->
+  <!-- <ExampleVFor /> -->
+  <!-- <ClassBind /> -->
+  <!-- <StyleBind /> -->
+  <!-- <ModelForm /> -->
+  <!-- <h1>Listado de Posts:</h1> -->
+  <!-- <CycleOption /> -->
+  <!-- <PostList /> -->
+  <!-- <ComputedExample /> -->
+  <!-- <ComputedExampleNew /> -->
+  <FacturaExample />
 </template>
 
 <script lang="ts" setup>
-import PostContainer from './components/PostContainer.vue';
-import CounterOption from './components/CounterOption.vue';
-import CounterComposition from './components/CounterComposition.vue';
-import ExampleSelective from './components/ExampleSelective.vue';
-import ExampleVFor from './components/ExampleV-For.vue';
-import ClassBind from './components/ClassBind.vue';
-import StyleBind from './components/styleBind.vue';
-import ModelForm from './components/ModelForm.vue';
+// import PostContainer from './components/PostContainer.vue';
+// import CounterOption from './components/CounterOption.vue';
+// import CounterComposition from './components/CounterComposition.vue';
+// import ExampleSelective from './components/ExampleSelective.vue';
+// import ExampleVFor from './components/ExampleV-For.vue';
+// import ClassBind from './components/ClassBind.vue';
+// import StyleBind from './components/styleBind.vue';
+// import ModelForm from './components/ModelForm.vue';
+// import CycleOption from './components/CycleOption.vue';
+// import PostList from './components/PostList.vue';
+// import ComputedExampleNew from './components/ComputedExampleNew.vue';
+import FacturaExample from './components/FacturaExample.vue';
 </script>
 
 <style>
