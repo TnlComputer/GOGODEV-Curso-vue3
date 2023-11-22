0719767a4a4181c66a3472af258eb1d195365799
Jorge Martinez <tnlcomputer@gmail.com>
2023-11-22 15:57:19 -0300

    Video 8 - Episodio 8 - Servicios

---
+++ services/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <PostList />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import PostList from './components/PostList.vue';
+export default defineComponent({
+  name: 'App',
+  components: {
+    PostList
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ services/src/components/PostList.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>Listado de Post</div>
+  <ul class='post-list'>
+    <li v-for='post in posts' :key='post.id'>{{ post.title }}</li>
+  </ul>
+</template>
+
+<!-- <script lang="ts" >
+import PostService from '@/services/PostService';
+import { defineComponent, onMounted } from 'vue';
+export default defineComponent({
+  name: 'PostList',
+  data() {
+    const service = new PostService()
+    const posts = service.getPosts()
+    return { posts, service }
+  },
+  async onMounted() {
+    await this.service.fetchAll()
+  }
+})
+
+</script> -->
+
+<!-- // <script lang="ts" >
+// import PostService from '@/services/PostService';
+// import { defineComponent, onMounted } from 'vue';
+// export default defineComponent({
+//   name: 'PostList',
+//   setup() {
+//     const service = new PostService()
+//     const posts = service.getPosts()
+
+//     onMounted(async () => {
+//       await service.fetchAll()
+//     })
+
+//     return { posts }
+//   }
+// })
+// </script>  -->
+
+<script lang="ts" setup >
+// import { defineComponent, onMounted } from 'vue';
+import { onMounted } from 'vue';
+import PostService from '@/services/PostService';
+const service = new PostService()
+const posts = service.getPosts()
+
+onMounted(async () => {
+  await service.fetchAll()
+})
+</script> 
+
+<style scoped>
+.post-list {
+  width: 95vw;
+  height: 75px;
+  padding: 20px;
+  text-align: left;
+  list-style-type: none;
+}
+
+.post-list li {
+  padding: 20px;
+  width: 100%;
+  border: 1px solid #ccc;
+}
+</style>
