e802efc25032dd0cbf2b58b0862823a457dc1883
Jorge Martinez <tnlcomputer@gmail.com>
2023-12-06 17:00:09 -0300

    video 14 - episodio 14 - Auth contra firebase

--- authentication/src/main.ts
+++ authentication/src/main.ts
@@ -2,6 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+import { initializeApp } from "firebase/app";
+import "bootstrap/dist/js/bootstrap.js"
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCy38F_lArHAh_aJV_l4x0bn9dJ_AgIhQI",
+  authDomain: "curso-vue-15d9b.firebaseapp.com",
+  projectId: "curso-vue-15d9b",
+  storageBucket: "curso-vue-15d9b.appspot.com",
+  messagingSenderId: "485511678730",
+  appId: "1:485511678730:web:daa656e37cee541cf6c8f3",
+  measurementId: "G-WRTQ0FV4SN"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 createApp(App).use(router).mount('#app')
---
+++ authentication/src/views/FirebaseView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model='email'>
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" v-model='password'>
+      </div>
+      <button type="submit" class="btn btn-primary mt-2" @click.prevent="authUser">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+
+let email = ref('')
+let password = ref('')
+
+const authUser = async () => {
+  const auth = getAuth()
+  signInWithEmailAndPassword(auth, email.value, password.value).then(() => {
+    alert('Exitos!')
+  })
+    .catch((error) => {
+      alert('Login incorrecto ' + error.message)
+
+    })
+}
+</script>
+
+<style>
+.container {
+  width: 400px;
+}
+</style>
