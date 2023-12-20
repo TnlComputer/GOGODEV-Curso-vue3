3c843622160afcf5eeee72a088e7d33281b8b040
Jorge Martinez <tnlcomputer@gmail.com>
2023-12-20 16:08:25 -0300

    video 19 - episodio 19 - Pinia

---
+++ global-pinia/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h3>Contador</h3>
+  <h3>{{ store.count }} x2 = {{ store.times2 }}</h3>
+  <button @click='store.increment(1)'>Increment</button>
+</template>
+
+<script lang="ts" setup>
+import { useCounterStore } from './store/counter';
+const store = useCounterStore()
+</script>
+
---
+++ global-pinia/src/store/counter.ts
@@ -0,0 +1,25 @@
+import { defineStore } from "pinia";
+
+export const useCounterStore = defineStore('counter',  {
+  state: () => {
+    return {count: 1}
+  },
+  getters: {
+    times2:(state) => state.count * 2
+  },
+  actions: {
+    increment(val = 1){
+      this.count += val 
+    }
+  }
+})
+
+// state
+
+// getters
+
+// mutattions y actions no hacen falta porque son los getters
+
+// actions
+
+// modules no se usan porque podemos hacer varios store
