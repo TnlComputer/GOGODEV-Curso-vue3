abad015b03fa504264e13930a87960c08998c436
Jorge Martinez <tnlcomputer@gmail.com>
2024-01-11 19:46:32 -0300

    video 27 - episodio 27 -watches (observadores)

--- persistency/src/App.vue
+++ persistency/src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <h3>{{ store.count }}</h3>
   <button @click='store.increment(1)'>Añadir</button>
 </template>
@@ -6,27 +6,85 @@
 <script lang='ts' setup>
 import { useExample } from '@/store/useExample';
 const store = useExample()
+
+/**
+ * con local storage
+ */
+
+store.init()
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+</style> -->
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+// watchers con option api
+<!-- <template>
+  <h3>Notas:</h3>
+  <input type="text" v-model='note'>
+  <button>Añadir</button>
+</template>
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script lang='ts'>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  data() {
+    return {
+      note: ''
+    }
+  },
+  watch: {
+    note(value, oldValue) {
+      // alert("Valor: " + value + " Antiguo valor: " + oldValue)
+      localStorage.setItem('note', value)
+    }
+  }
+}) -->
+
+// watcher composition api
+<!-- <template>
+  <h3>Notas:</h3>
+  <input type="text" v-model='note'>
+  <button>Añadir</button>
+</template>
+
+<script lang='ts'>
+import { defineComponent, ref, watchEffect } from 'vue'
+export default defineComponent({
+  setup() {
+    const note = ref('')
+    watchEffect(() => alert("Se ha modificado el valor: " + note.value))
+    return {
+      note
+    }
+  }
+}) -->
+
+// watcher script setup con watchEffect
+<!-- <template>
+  <h3>Notas:</h3>
+  <input type="text" v-model='note'>
+  <button>Añadir</button>
+</template>
+
+<script lang='ts' setup>
+import { ref, watchEffect } from 'vue'
+const note = ref('')
+watchEffect(() => alert("Se ha modificado el valor: " + note.value)) -->
+
+// watcher script setup con watch
+<!-- <template>
+  <h3>Notas:</h3>
+  <input type="text" v-model='note'>
+  <button>Añadir</button>
+</template>
+
+<script lang='ts' setup>
+import { ref, watch } from 'vue'
+const note = ref('')
+watch(note, () => alert("Se ha modificado el valor: " + note.value)) -->
+
+
+</script>
+
+<style></style>
--- persistency/src/store/useExample.ts
+++ persistency/src/store/useExample.ts
@@ -1,22 +1,45 @@
 import { defineStore } from 'pinia';
 
+// export const useExample = defineStore('useExample',{
+//   state: () => {
+//     return {
+//       count : 1
+//     }
+//   },
+//   actions:{
+//     increment(val = 1) {
+//       this.count += val
+//     }
+//   },
+//   //persistencia con pinia
+//   // persist: true  // persist facil
+
+//   // persistencia con pinia diciendole que persiste y que campos
+//   persist: {
+//     storage: sessionStorage,
+//     paths: ['count']
+//   }
+// })
+
+
 export const useExample = defineStore('useExample',{
+  //persistencia con localStorge
   state: () => {
     return {
       count : 1
     }
   },
   actions:{
-    increment(val = 1) {
+    init() {
+      const initCount = localStorage.getItem('count')
+      if (initCount){
+        this.count = parseInt(initCount)
+      }
+    },
+    increment(val = 1) { 
       this.count += val
+      localStorage.setItem('count', this.count.toString() )
     }
   },
-  //persistencia con pinia
-  // persist: true  // persist facil
 
-  // persistencia con pinia diciendole que persiste y que campos
-  persist: {
-    storage: sessionStorage,
-    paths: ['count']
-  }
 })
