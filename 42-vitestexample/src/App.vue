6e5594814efb97ecb16cffd39b97a6b660693444
Jorge Martinez <tnlcomputer@gmail.com>
2024-02-04 18:01:24 -0300

    video 42 - episodio 42 - vitest - jest

---
+++ 42-vitestexample/src/App.vue
@@ -0,0 +1,13 @@
+
+<template>
+  <h1>{{ counter }}</h1>
+  <button @click='increment'>Increment</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const counter = ref(0)
+const increment = () => counter.value++
+</script>
+
+<style></style>
